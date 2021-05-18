2ba8f938f35f2fbfe25b42b39f65d56522a0198f
Ryan Hasinsky <hasinsky3@gmail.com>
2021-05-17 19:54:47 -0700

    More function

--- js/gallery.js
+++ js/gallery.js
@@ -4,14 +4,13 @@
 
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage  = document.querySelector("#gallery-photo img");
+  let mainImage = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
-      mainImage.setAttribute("alt", FILL_IN);
     });
   });
 }
