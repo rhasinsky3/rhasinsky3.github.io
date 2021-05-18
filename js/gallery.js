579bc54a0836a7899114cfe449f460a0ca876a32
Ryan Hasinsky <hasinsky3@gmail.com>
2021-05-17 20:17:59 -0700

    Done

--- js/gallery.js
+++ js/gallery.js
@@ -4,17 +4,17 @@
 
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImage  = document.querySelector("#gallery-photo img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
-      // Set clicked image as main image.
+      // Set clicked image as display image.
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
 
       // Change which image is current.
       document.querySelector(".current").classList.remove("current");
-      thumbnail.parentNode.classSelector("current");
+      thumbnail.parentNode.classList.add("current");
     });
   });
 }
