bcf4b61cb982995bac6040d88d6efcc911655496
Ryan Hasinsky <hasinsky3@gmail.com>
2021-05-17 19:45:55 -0700

    Added function to the gallery page

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,17 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage  = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", FILL_IN);
+    });
+  });
 }
